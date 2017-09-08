f6a1f9180df6de8ecbbc17019cd1d4e8ad92c9c1
Brandon Thomas <echelon@gmail.com>
2017-09-08 01:08:19 -0400

    rename, docs

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -1,22 +1,26 @@
 //! This module contains various algorithms for point and color transforms.
 
-/// A coordinate transformation.
-struct CoordinateTransformer<S, D> {
-  pub min_src: S,
-  pub max_src: S,
-  pub min_dest: D,
-  pub max_dest: D,
+/// Scaler performs a coordinate transformation from a source range to a target
+/// range.
+struct Scaler<S, D> {
+  min_src: S,
+  max_src: S,
+  min_dest: D,
+  max_dest: D,
 }
 
-macro_rules! coordinate_impl {
+macro_rules! scaler_impl {
   ($src_t: ty, $dest_t: ty) => {
-    impl CoordinateTransformer<$src_t, $dest_t> {
+    impl Scaler<$src_t, $dest_t> {
 
-      /// Construct a new CoordinateTransformer.
+      /// Construct a new Scaler.
+      /// * `min_src` - minimum of the source range
+      /// * `max_src` - maximum of the source range
+      /// * `min_dest` - minimum of the output range
+      /// * `max_dest` - maximum of the output range
       pub fn new(min_src: $src_t, max_src: $src_t, min_dest: $dest_t,
-                 max_dest: $dest_t) -> CoordinateTransformer<$src_t, $dest_t> {
-
-        CoordinateTransformer {
+                 max_dest: $dest_t) -> Scaler<$src_t, $dest_t> {
+        Scaler {
           min_src: min_src,
           max_src: max_src,
           min_dest: min_dest,
@@ -24,6 +28,8 @@ macro_rules! coordinate_impl {
         }
       }
 
+      /// Scale a number from the source range to the target output dimensions.
+      /// * `num` - the number to be scaled.
       #[inline]
       pub fn scale_number(&self, num: $src_t) -> $dest_t {
         let numerator = num.saturating_sub(self.min_src);
@@ -42,23 +48,23 @@ macro_rules! coordinate_impl {
 // There has to be a cleaner way of doing this.
 // Only implemented for the widths used in laser projection.
 
-coordinate_impl!(u16, u16);
-coordinate_impl!(u16, u32);
-coordinate_impl!(u16, i16);
-coordinate_impl!(u16, i32);
-coordinate_impl!(u32, u16);
-coordinate_impl!(u32, u32);
-coordinate_impl!(u32, i16);
-coordinate_impl!(u32, i32);
+scaler_impl!(u16, u16);
+scaler_impl!(u16, u32);
+scaler_impl!(u16, i16);
+scaler_impl!(u16, i32);
+scaler_impl!(u32, u16);
+scaler_impl!(u32, u32);
+scaler_impl!(u32, i16);
+scaler_impl!(u32, i32);
 
-coordinate_impl!(i16, u16);
-coordinate_impl!(i16, u32);
-coordinate_impl!(i16, i16);
-coordinate_impl!(i16, i32);
-coordinate_impl!(i32, u16);
-coordinate_impl!(i32, u32);
-coordinate_impl!(i32, i16);
-coordinate_impl!(i32, i32);
+scaler_impl!(i16, u16);
+scaler_impl!(i16, u32);
+scaler_impl!(i16, i16);
+scaler_impl!(i16, i32);
+scaler_impl!(i32, u16);
+scaler_impl!(i32, u32);
+scaler_impl!(i32, i16);
+scaler_impl!(i32, i32);
 
 #[cfg(test)]
 mod test {
@@ -66,27 +72,24 @@ mod test {
 
   #[test]
   fn spot_check() {
-    let mut ct =
-      CoordinateTransformer::<u16, u16>::new(0u16, 100u16, 0u16, 255u16);
+    let mut sc = Scaler::<u16, u16>::new(0u16, 100u16, 0u16, 255u16);
 
-    assert_eq!(0, ct.scale_number(0));
-    assert_eq!(127, ct.scale_number(50));
-    assert_eq!(255, ct.scale_number(100));
+    assert_eq!(0, sc.scale_number(0));
+    assert_eq!(127, sc.scale_number(50));
+    assert_eq!(255, sc.scale_number(100));
 
-    let mut ct =
-      CoordinateTransformer::<u16, i16>::new(0u16, 100u16, -100i16, 100i16);
+    let mut sc = Scaler::<u16, i16>::new(0u16, 100u16, -100i16, 100i16);
 
-    assert_eq!(-100, ct.scale_number(0));
-    assert_eq!(0, ct.scale_number(50));
-    assert_eq!(100, ct.scale_number(100));
+    assert_eq!(-100, sc.scale_number(0));
+    assert_eq!(0, sc.scale_number(50));
+    assert_eq!(100, sc.scale_number(100));
 
-    let mut ct =
-      CoordinateTransformer::<i16, u16>::new(0i16, 10i16, 0u16, 1000u16);
+    let mut sc = Scaler::<i16, u16>::new(0i16, 10i16, 0u16, 1000u16);
 
-    assert_eq!(0, ct.scale_number(0));
-    assert_eq!(200, ct.scale_number(2));
-    assert_eq!(500, ct.scale_number(5));
-    assert_eq!(900, ct.scale_number(9));
-    assert_eq!(1000, ct.scale_number(10));
+    assert_eq!(0, sc.scale_number(0));
+    assert_eq!(200, sc.scale_number(2));
+    assert_eq!(500, sc.scale_number(5));
+    assert_eq!(900, sc.scale_number(9));
+    assert_eq!(1000, sc.scale_number(10));
   }
 }
